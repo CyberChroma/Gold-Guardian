098d5f5561a9c1f5277e8e2fea96f5e345146d2c
Lama <labbas@uwo.ca>
2022-07-06 02:15:35 -0400

    Projectile working. To do: - tweak to make projectile continuous rather than collapsy - remove dependence on axis/stationary direction for projectile's spawn

--- Gold Guardian/Assets/Scripts/Projectile.cs
+++ Gold Guardian/Assets/Scripts/Projectile.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
+    [SerializeField] private float lifetime;
+
     void Update()
     {
-        
+        Destroy(gameObject, lifetime);
     }
 }
--- Gold Guardian/Assets/Scripts/UnitRange.cs
+++ Gold Guardian/Assets/Scripts/UnitRange.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class UnitRange : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
+    [SerializeField] private GameObject projectile;
+    [SerializeField] private Transform projectileSpawner;
+    [SerializeField] private float launchVelocity;
+    [SerializeField] private float cooldown;
+
+    private float lifetimeCoolDown = Mathf.Infinity;
+
     void Update()
     {
-        
+        if (lifetimeCoolDown > cooldown)
+        {
+            GameObject projectileLaunch = Instantiate(projectile, projectileSpawner.position, projectileSpawner.rotation);
+            projectileLaunch.GetComponent<Rigidbody>().AddRelativeForce(new Vector3(launchVelocity, 0, 0));
+            
+            lifetimeCoolDown = 0;
+        }
+        lifetimeCoolDown += Time.deltaTime;
     }
 }
