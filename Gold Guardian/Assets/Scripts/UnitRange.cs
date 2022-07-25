ba2d3864fa84360f4f46b79db5ec25ce83ab4a79
Lama <labbas@uwo.ca>
2022-07-24 21:50:38 -0400

    Minor changes

--- Gold Guardian/Assets/Scripts/UnitRange.cs
+++ Gold Guardian/Assets/Scripts/UnitRange.cs
@@ -20,13 +20,17 @@ public class UnitRange : MonoBehaviour
 
     void Update()
     {
+        Shoot();
+    }
 
+    private void Shoot()
+    {
         if (lifetimeCoolDown > cooldown)
         {
             Projectile projectileLaunch = Instantiate(projectile, projectileSpawner.position, projectileSpawner.rotation) as Projectile;
             projectileLaunch.transform.position = projectileSpawner.position;
             Physics.IgnoreCollision(projectileLaunch.GetComponent<BoxCollider>(), unitCollider);
-            
+
 
             Rigidbody body = projectileLaunch.GetComponent<Rigidbody>();
             body.AddRelativeForce(new Vector3(projectileSpeed, 0, 0), ForceMode.Impulse);
