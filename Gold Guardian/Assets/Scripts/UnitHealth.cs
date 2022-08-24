decdfb49be491a89b5cc3a43441878ed0e7b4746
Brandon Howe <cyberchroma@hotmail.com>
2022-08-24 16:40:35 -0400

    Added Health Bars To Creatures

--- Gold Guardian/Assets/Scripts/UnitHealth.cs
+++ Gold Guardian/Assets/Scripts/UnitHealth.cs
@@ -1,23 +1,42 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UnitHealth : MonoBehaviour {
+    public int startHealth;
 
-    public int health;
+    private int curHealth;
+    private UnitType unitType;
+    private Slider healthSlider;
 
     // Start is called before the first frame update
     void Start() {
-        
-    }
-
-    // Update is called once per frame
-    void Update() {
-        
+        curHealth = startHealth;
+        unitType = GetComponent<UnitType>();
+        healthSlider = GetComponentInChildren<Slider>();
+        healthSlider.maxValue = startHealth;
+        healthSlider.value = startHealth;
+        Image sliderBackground = healthSlider.transform.GetChild(1).GetChild(0).GetComponent<Image>();
+        if (unitType.type == UnitType.Type.enemy) {
+            sliderBackground.color = Color.red;
+        } else {
+            sliderBackground.color = Color.green;
+        }
     }
 
     public void Damage(int damage) {
-        health -= damage;
-        if (health <= damage) {
+        curHealth -= damage;
+        healthSlider.maxValue = curHealth;
+        if (curHealth <= 0) {
+            curHealth = 0;
             Destroy(gameObject);
+        }
+    }
+
+    public void Heal(int health) {
+        curHealth += health;
+        healthSlider.maxValue = curHealth;
+        if (curHealth > startHealth) {
+            curHealth = startHealth;
         }
     }
 }
